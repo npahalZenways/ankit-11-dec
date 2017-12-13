504f3aec6ba2b119fddf68c6f1fe6c2ea8fd3d23
npahal <pahal.nitin@gmail.com>
2017-12-13 13:25:10 +0530

    December 13 2017

--- my-project/src/app/app.component.ts
+++ my-project/src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'akufuyfpp';
+  title = 'My APP';
+  red="blue";
+  redClass = "blueClass";
+  person = {
+    name:'',
+    phone:''
+  }
+  imageSource = "https://imagejournal.org/wp-content/uploads/bb-plugin/cache/23466317216_b99485ba14_o-panorama.jpg";
+  changeTitle(title){
+    this.title = title;
+  }
+  changeClass(){
+    this.redClass = 'redClass';
+  }
+  print(title){
+    console.log(title);
+  }
+  changeImg(source){
+    this.imageSource = source;
+  }
 }
